41da96f41180c9c296094fca8f364cb73065bd2f
bettyght1 <bdesta@miu.edu>
2021-12-29 00:43:44 -0600

    First commit

---
+++ src/main/java/com/springjenkins/springjenkins/SpringJenkinsApplication.java
@@ -0,0 +1,15 @@
+package com.springjenkins.springjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.logging.Logger;
+
+@SpringBootApplication
+public class SpringJenkinsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJenkinsApplication.class, args);
+        System.out.println("Application running.....");
+    }
+
+}
