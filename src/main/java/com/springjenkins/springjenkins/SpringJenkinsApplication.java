b03e320a392994fb6592d4b02d7740f4ccc970eb
bettyght1 <bdesta@miu.edu>
2021-12-29 14:39:20 -0600

    Error file

--- src/main/java/com/springjenkins/springjenkins/SpringJenkinsApplication.java
+++ src/main/java/com/springjenkins/springjenkins/SpringJenkinsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringJenkinsApplication {
 
-    public static void main(String[] args) {
+    public static  main(String[] args) {
         SpringApplication.run(SpringJenkinsApplication.class, args);
         System.out.println("Application running.....");
     }
